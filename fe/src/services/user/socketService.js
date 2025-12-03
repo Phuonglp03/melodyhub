8e8dd1df5a44a2abdd25d95e812fbf5e551d205f
Phuonglp03 <tvp141003@gmail.com>
2025-12-03 14:44:56 +0700

    fix socket link

--- fe/src/services/user/socketService.js
+++ fe/src/services/user/socketService.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 import { store } from "../../redux/store";
 // URL của server (cổng Express/Socket.IO)
 const SOCKET_URL =
-  process.env.REACT_APP_SOCKET_URL || "https://api.melodyhub.website";
+  process.env.REACT_APP_SOCKET_URL ;
 console.log("[Socket.IO] SOCKET_URL resolved to:", SOCKET_URL);
 
 let socket;
